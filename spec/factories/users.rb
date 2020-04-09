1e268ad36cd29019ee6ac45f0a6a3f3abb46a253
William Meighan <williamdmeighan@gmail.com>
2020-04-08 20:35:59 -0600

    user factories

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    uid { "" }
+    first_name { "MyString" }
+    last_name { "MyString" }
+    google_token { "MyString" }
+    email { "MyString" }
+  end
+end
