40b650d5f06adee043c232dacd8131a63152893b
William Meighan <williamdmeighan@gmail.com>
2020-04-08 20:33:42 -0600

    create user model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  validates_presence_of :first_name,
+                        :last_name,
+                        :google_token,
+                        :email
+
+  validates_uniqueness_of :email
+end
