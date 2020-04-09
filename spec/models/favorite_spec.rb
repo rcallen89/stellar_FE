036ad4d24a6ea1d8bcbde7f673069fc239a1b0e5
William Meighan <williamdmeighan@gmail.com>
2020-04-09 16:31:02 -0600

    Create favorite model spec

---
+++ spec/models/favorite_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Favorite, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :name}
+  end
+  describe "relationships" do
+    it {should belong_to :user}
+  end
+end
