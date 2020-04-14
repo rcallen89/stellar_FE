cbb085b25b86530a00374d306c3f546320a0535a
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-14 12:00:42 -0600

    Test that a user can search for something other than a planet and they will be redirected to the home page if they search for something the api doesnt have

--- spec/features/search/solar_system_search_spec.rb
+++ spec/features/search/solar_system_search_spec.rb
@@ -2,23 +2,52 @@ require 'rails_helper'
 
 RSpec.describe 'As a user' do
   describe 'from any page' do
-    before do
-      Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:google_oauth2]
+    before :each do
+      @user = User.create!(uid: 13,
+                          first_name: "Meghan",
+                          last_name: "Stovall",
+                          google_token: "abc123def456ghi789",
+                          email: "meghan.stovall@gmail.com")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
     end
 
     it 'I can search for an included solar system object', :vcr do
-      stub_omniauth
       visit '/'
-      click_button 'Sign-In with Google'
-      user = User.last
-
-      expect(current_path).to eq('/profile')
-      expect(page).to have_content("Hi, #{user.first_name}!")
 
       fill_in "search", with: "Jupiter"
       click_button 'Search'
 
       expect(current_path).to eq('/search')
+
+      within ".solar-system-data" do
+        expect(first('#discovered_by').text).to_not be_empty
+        expect(first('#discovered_on').text).to_not be_empty
+        expect(first('#mass').text).to_not be_empty
+        expect(first('#volume').text).to_not be_empty
+        expect(first('#density').text).to_not be_empty
+        expect(first('#gravity').text).to_not be_empty
+      end
+    end
+
+    it "can search for something thats not a planet", :vcr do
+      visit '/'
+
+      fill_in "search", with: "Vesta"
+      click_button 'Search'
+
+      expect(current_path).to eq('/search')
+      expect(page).to have_content("Planet? No")
+    end
+
+    it "user gets redirected if they enter an unknown body", :vcr do
+      visit '/'
+
+      fill_in "search", with: "Pluto"
+      click_button 'Search'
+
+      expect(current_path).to eq('/')
+      expect(page).to have_content("We're sorry, we could not find what you were looking for.")
     end
   end
 end
