8fdf4a92a0cb474b7fe681bf2ab96b3d97c651e6
William Meighan <williamdmeighan@gmail.com>
2020-04-14 19:39:07 -0600

    Create favorites controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,35 @@
 class FavoritesController < ApplicationController
-  
+
   def index
+    current_user
   end
+
+  def show
+    @favorite = Favorite.find(params[:favorite_id])
+    @object = SolarSystemFacade.new(@favorite.name)
+  end
+
+  def new
+  end
+
+  def create
+    @favorite = current_user.favorites.create(favorite_params)
+    @favorite.save
+    flash[:success] = "#{@favorite.name} has been saved to favorites"
+    redirect_to "/favorites"
+  end
+
+  def destroy
+    favorite = Favorite.find(params[:favorite_id])
+    favorite.destroy
+    flash[:success] = "#{favorite.name} has been removed from your favorites."
+    redirect_to '/favorites'
+  end
+
+  private
+
+    def favorite_params
+      params.permit(:name)
+    end
 
 end
